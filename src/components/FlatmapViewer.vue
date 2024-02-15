15ddbf74d375ad2d987e60a26084a94be65dcf84
Samantha Kraft <samantha@brb-11017.apn.wlan.private.upenn.edu>
2024-02-15 14:53:19 -0500

    flatmap setup

---
+++ src/components/FlatmapViewer.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="fill">
+        Filters go here
+    </div>
+    <div class="flatmap-viewer">
+     
+    </div>
+    <el-table :data="TableData" class="table-of-images">
+        <el-table-column prop="name" label="Name"/>
+        <el-table-column prop="size" label="Size"/>
+        <el-table-column fixed="right" label="Img">
+            <template #default="scope">
+                <el-button link type="primary" size="small" @click="selectImage(scope.$index)">Open</el-button>
+            </template> 
+        </el-table-column>
+    </el-table>
+</template>
+<script setup>
+  import { ref} from "vue";
+  import BranchSlider from "../devComponents/BranchSlider.vue";
+  import {Dataset} from '../assets/Model';
+
+  const baseUrl = './imgs/'
+  let TableData = ref();
+  let Location ="";
+
+  const props = defineProps({
+            dataList:{
+                type:Array,
+                required:false
+            },
+        })
+
+function selectImage(index){
+
+    let img = TableData.value[index].path;
+
+}
+function selectLocation(location){
+    Location=location;
+    //make call using location
+    fetch('./dataByLocation.json')
+        .then((response) => response.json())
+        .then((json) => buildDataTable(Object.assign(new Dataset(json)).Imgs),
+        );
+}
+
+function buildDataTable(Imgs){
+    let _tempArr=[];
+    Imgs.forEach((img)=>{
+        let column = {
+            name:img.ImgName,
+            size: "...",
+            path: img.Path
+        }
+        _tempArr.push(column);
+    })
+    TableData.value=_tempArr;
+}
+</script>
+<style scoped lang="scss">
+    .flatmap-viewer{
+        display:flex;
+        justify-content: center;
+        margin-bottom:10px;
+    }
+    .open-image{
+        color:#8300BF;
+    }
+
+</style>
