6e7ab350ff148127987d674c1a3648f2b6ccde2f
Samantha Kraft <kraftsam@gmail.com>
2024-04-26 10:22:34 -0400

    remove plugin from main.js

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
 </template>
 <script setup lang="ts">
 import { ref, onMounted, watch} from 'vue';
-//import SparcDashboard from "./components/SparcDashboard.vue"
-//import "../tailwind/output.css"
+import SparcDashboard from "./components/SparcDashboard.vue"
+
 
 
   onMounted(() => {
--- src/main.js
+++ src/main.js
@@ -1,39 +1,41 @@
 import './assets/main.css'
 
- import { createApp, defineAsyncComponent } from 'vue'
+import { createApp, defineAsyncComponent } from 'vue'
 
 import { createPinia } from 'pinia'
 import DesignSystemComponentsPlugin from 'sparc-design-system-components-2'
+import '../node_modules/sparc-design-system-components-2/dist/style.css';
 import mitt from 'mitt'
  import App from './App.vue'
  //import DashboardPlugin from "../dist/index"
- import DashboardPlugin from './DashboardPlugin'
+ //import DashboardPlugin from './DashboardPlugin'
 import { useGlobalVarsStore } from './stores/globalVars'
+import "../tailwind/output.css"
 
  const app = createApp(App);
 app.use(DesignSystemComponentsPlugin);
 app.use(createPinia());
-app.use(DashboardPlugin);
-//const emitter = mitt();
-//app.provide('emitter', emitter); 
+//app.use(DashboardPlugin);
+const emitter = mitt();
+app.provide('emitter', emitter); 
 
-// const componentMap = [
-//     'ImageSelector',
-//     'LocationNav',
-//     'FlatmapViewer',
-//     'BiolucidaViewer',
-//     'QDBChart'
-// ]
-// componentMap.forEach(comp=>{
-//     const asyncComponent = defineAsyncComponent(() => import(`./components/${comp}.vue`)); 
-//     app.component(comp, asyncComponent);
-// })
+const componentMap = [
+    'ImageSelector',
+    'LocationNav',
+    'FlatmapViewer',
+    'BiolucidaViewer',
+    'QDBChart'
+]
+componentMap.forEach(comp=>{
+    const asyncComponent = defineAsyncComponent(() => import(`./components/${comp}.vue`)); 
+    app.component(comp, asyncComponent);
+})
 
 
 //add list of components to add componet drop down
-//const globalVars = useGlobalVarsStore();
-//globalVars.componentList = componentMap;
+const globalVars = useGlobalVarsStore();
+globalVars.componentList = componentMap;
 
-//app.use(DesignSystemComponentsPlugin);
+app.use(DesignSystemComponentsPlugin);
 
 app.mount('#app');
